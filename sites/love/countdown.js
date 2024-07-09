fb82ad55db4ad0c87697f68ebdb900008c50d57c
James Hall <james.hall01@outlook.com>
2024-07-09 21:35:12 +0100

    update countdown

--- sites/love/countdown.js
+++ sites/love/countdown.js
@@ -3,26 +3,27 @@ var countDownDate = new Date("Apr 9, 2024 17:55:55").getTime();
 // Update the count down every 1 second
 var interval = setInterval(function() {
   var text = document.getElementById("countdown");
-  let heart = document.getElementById('heart');
-  let active = heart.classList.contains("active");
-
-  if (!active) {
-    text.style.opacity = 0;
-    return;
-  }
-  text.style.opacity = 1;
 
   var now = new Date().getTime();
 
   var distance = now - countDownDate;
 
-  // Time calculations for months, days, hours, minutes and seconds
-  var months = Math.floor(distance / (1000 * 60 * 60 * 24) / 30 )
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24) - (months * 30));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let formatter = new Intl.NumberFormat();
 
-  text.innerHTML = months + " months | " + days + " days | " + hours + " hours | "
-  + minutes + " mins | " + seconds + " secs";
+  // Time calculations for months, days, hours, minutes and seconds
+  var months = Math.floor(distance / (1000 * 60 * 60 * 24) / 30 );
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24) - (months * 30));
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  var days = months * 30;
+  var hours = days * 24;
+  var minutes = hours * 60;
+  var seconds = Math.floor(distance / 1000);
+
+  let breakText = "<br>";
+
+  text.innerHTML = formatter.format(months) + " months" + breakText + formatter.format(days) + " days" + breakText + formatter.format(hours) + " hours" + breakText
+  + formatter.format(minutes) + " mins" + breakText + formatter.format(seconds) + " secs";
 }, 1000);
--- sites/love/script.js
+++ sites/love/script.js
@@ -2,7 +2,21 @@ function initialise() {
     let heart = document.getElementById('heart');
     heart.onclick = function(){
         heart.classList.toggle('active');
-    }    
+        updateCountdown();
+    }
+}
+
+function updateCountdown() {
+    var text = document.getElementById("countdown");
+
+    let heart = document.getElementById('heart');
+    let active = heart.classList.contains("active");
+  
+    if (!active) {
+      text.style.opacity = 0;
+      return;
+    }
+    text.style.opacity = 1;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
