f60327b4b158321942a37a61fd1afc7a4dee8de9
James Hall <james.hall01@outlook.com>
2024-08-03 22:22:07 +0100

    update grid for smaller widths

--- script/windows.js
+++ script/windows.js
@@ -1,3 +1,5 @@
+const MAX_WIDTH = 787;
+
 function initialise() {
     let sections = document.getElementsByClassName("section-window-container");
 
@@ -9,6 +11,11 @@ function initialise() {
 }
 
 function updateGrid(grid) {
+    if (window.outerWidth < MAX_WIDTH) {
+        grid.style.gridTemplateColumns = 'repeat(1, 1fr)';
+        return;
+    }
+
     let count = grid.childElementCount;
 
     grid.style.gridTemplateColumns = `repeat(${count}, 1fr)`;
@@ -18,4 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('load', () => {
         initialise();
     });
+
+    window.addEventListener('resize', () => {
+        initialise();
+    })
 });
