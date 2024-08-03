19433e197527b0cb398cce9a8fb50c21d4fd18af
James Hall <james.hall01@outlook.com>
2024-08-03 23:54:12 +0100

    birthday site

--- sites/love/countdown.js
+++ sites/love/countdown.js
@@ -1,12 +1,31 @@
-var countDownDate = new Date("Apr 9, 2024 17:55:55").getTime();
+var dateMap = new Map();
+
+dateMap.set('countdown-anniversary', new Date("Apr 9, 2024 17:55:55").getTime());
+dateMap.set('countdown-anniversary-next', new Date("Aug 9, 2024 17:55:55").getTime());
+dateMap.set('countdown-first-hangout', new Date("Apr 4, 2024 17:55:55").getTime());
+dateMap.set('countdown-first-meet', new Date("Mar 13, 2024 17:55:55").getTime());
+dateMap.set('countdown-first-date', new Date("Mar 30, 2024 17:55:55").getTime());
 
 // Update the count down every 1 second
 var interval = setInterval(function() {
-  var text = document.getElementById("countdown");
+  for (const [id, date] of dateMap.entries()) {
+    updateCountdown(id, date);
+  }
+}, 1000);
+
+function updateCountdown(textId, date) {
+  var text = document.getElementById(textId);
+  if (text == null) {
+    return;
+  }
 
   var now = new Date().getTime();
 
-  var distance = now - countDownDate;
+  var distance = now - date;
+
+  if (distance < 0) {
+    distance = date - now;
+  }
 
   let decimalFormatted = new Intl.NumberFormat("en", {
     maximumFractionDigits : 1
@@ -18,21 +37,6 @@ var interval = setInterval(function() {
   })
 
   // Time calculations for months, days, hours, minutes and seconds
-
-  // 1.0
-  // var months = Math.floor(distance / (1000 * 60 * 60 * 24) / 30 );
-  // var days = Math.floor(distance / (1000 * 60 * 60 * 24) - (months * 30));
-  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // 2.0
-  // var days = months * 30;
-  // var hours = days * 24;
-  // var minutes = hours * 60;
-  // var seconds = Math.floor(distance / 1000);
-
-  // CURRENT
   var seconds = Math.floor(distance / 1000);
   var minutes = seconds / 60;
   var hours = minutes / 60;
@@ -43,4 +47,4 @@ var interval = setInterval(function() {
 
   text.innerHTML = roundedFormat.format(months) + " months" + breakText + roundedFormat.format(days) + " days" + breakText + decimalFormatted.format(hours) + " hours" + breakText
   + roundedFormat.format(minutes) + " mins" + breakText + decimalFormatted.format(seconds) + " secs";
-}, 1000);
+}
